85ef883192ecfe24ff697b3ad3b79cd070247274
catalinaaguayo <c.aguayo02@ufromail.cl>
2016-09-10 15:51:24 -0300

    Update ProyectoSemestral.java

--- ProyectoSemestral.java
+++ ProyectoSemestral.java
@@ -1,3 +1,4 @@
+//hola estoy pobando esto porque no me lo quiero hechar 
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
