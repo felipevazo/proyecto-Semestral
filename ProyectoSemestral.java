b1026e571bbf7d502057310e92ec9724135d5788
catalinaaguayo <c.aguayo02@ufromail.cl>
2016-09-26 17:12:51 -0300

    Update ProyectoSemestral.java

--- ProyectoSemestral.java
+++ ProyectoSemestral.java
@@ -1,7 +1,8 @@
-//hola estoy pobando esto porque no me lo quiero hechar, holaaaaa
-
-package proyecto.semestral;
+// ÚLTIMA ACTUALIZACIÓN EN CONJUNTO PARA PRIMER AVANCE 26/09/2016
+package proyectosemestral;
 import java.util.*;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class ProyectoSemestral {
 static Scanner entrada=new Scanner(System.in); 
@@ -21,10 +22,9 @@ static boolean cierreTope=false;
     {
         return cierreTope;
     }
-    public static void setCierreTope(boolean cierreTope)
-    {
-        this.cierreTope=cierreTope;
-        
+
+    public static void setCierreTope(boolean cierreTope) {
+        ProyectoSemestral.cierreTope = cierreTope;
     }
 
     public static double getTopeTrabajo() {
@@ -87,20 +87,18 @@ static boolean cierreTope=false;
         contador(topeTrabajo);
         actividades(listaActividades.size());
         contador(topeDescanso); 
-    // AGREGAR COMENTARIO diferente segun situacion.
+        // AGREGAR COMENTARIO diferente segun situacion.
         }
         System.out.println("Tope de horas definidas maximas, muchas gracias por usar el programa y que tenga un buen dia.");
     }
     
-    //¿¿¿este contador es el mismo del que esta en menu? el de contador(trabajo)?????
-    //R: si, se puede usar en ambos contextos
     private static void contador(double tope) throws InterruptedException
     {
         double pDecimal=tope - (int)tope;
         int minTotales=(int)tope*60+(int)(pDecimal*60);
         int minutos=0;
         int segundos=0;
-        boolean ciclo =true;
+        boolean ciclo=true;
         while(minutos<minTotales||cierreTope==false)
         {
             //En resumen, el ciclo cortara cuando se cumplan los minutos decididos por usuario.
@@ -115,17 +113,15 @@ static boolean cierreTope=false;
             }
             if(getContadorTope()==topeTotal)
             {
-                cierreTope==true;
+                cierreTope=true;
             }
         }
             
-        
-    }
-     //Una vez transcurridas 2 horas, el sistema lanzará una ventana emergente
+        //Una vez transcurridas 2 horas, el sistema lanzará una ventana emergente
         System.out.println("Has pasado 2 horas sentado en el computador trabajando, toma un descanso.");
-        System.out.println("El sistema te entregará una actividad entre tus elegidas para realizar.");
-        
+        System.out.println("El sistema te entregará una actividad entre tus elegidas para realizar."); 
     }
+    
     private static void actividades(int numeroactividades)
     {
         //¿¿¿¿que es el trato????
@@ -149,25 +145,26 @@ static boolean cierreTope=false;
             default:
                 System.out.println("Bienvenido al bonus, sientese, mire al techo y piense en su quehacer de la semana durante unos minutos.");
                 break;
+        }
               
-                if(buscaLinea(listaActividades.get(i-1),".*parrafos.*")
-               {
+                if(buscaLinea(listaActividades.get(i-1),".*parrafos.*"))
+                {
                     System.out.println("Se abrira un parrafo de un libro aleatorio, para que ud disfrute de una distraccion sutil.");
-                        //Aqui se abre el archivo de texto estatico 
+                    //Aqui se abre el archivo de texto estatico 
                 }
-                else if(buscaLinea(listaActividades.get(i-1),".*musica.*")
+                else if(buscaLinea(listaActividades.get(i-1),".*musica.*"))
                 {
                      System.out.println("a continuacion sonara musica relajante, mientras aparecen un par de imagenes para ayudar a la distraccion.");
                 }
-                else if(buscaLinea(listaActividades.get(i-1),".*cafe.*")
+                else if(buscaLinea(listaActividades.get(i-1),".*cafe.*"))
                 {
                     System.out.println("Preparese un cafe con calma, prenda su cigarro(o solamente el cafe, como ud prefiera) y tome su tiempo para degustarlo.");
                 }
-                else if(buscaLinea(listaActividades.get(i-1),".*caminata.*")
+                else if(buscaLinea(listaActividades.get(i-1),".*caminata.*"))
                 {
                     System.out.println("Momento de una caminata, de una vuelta por su calle, tome aire y disfrute de la vista.");
                 }
-                else if(buscaLinea(listaActividades.get(i-1),".*canciones.*);
+                else if(buscaLinea(listaActividades.get(i-1),".*canciones.*"))
                 {
                     System.out.println("Disfrute de interpretar sus canciones favoritas en su instrumento deseado.");
                     //Se podria añadir una pagina web con acordes, para ayudar a pensar cosas que tocar.
@@ -176,50 +173,49 @@ static boolean cierreTope=false;
                 {
                     System.out.println("Momento de hacer literalmente.. nada, apague la pantalla unos minutos, mire a la nada y piense, disfrute de este momento de su vida haciendo absolutamente nada.");
                 }
-        }
+        
         //Avanzando, esto debería tomar mas opciones, desplegar en la misma pantalla actividades a realizar.
         //O directamente un cafe con un cigarro, a gusto del usuario(dentro de las opciones que el mismo programa barajará).
     } 
-        private static void actividades()
+    
+        private static void seleccionActividades()
         {
             //Se crea una variable resp por mientras que aprendemos a interactuar con ventanas.
             System.out.println("Imaginese que tiene un día libre, ¿Cuál de las siguientes actividades haría?");
             System.out.println("(Para seleccionar haga click sobre la imagen)");
             //Actividades tentativas
-            System.out.println("Imagen con parrafos de libro")
-            resp=entrada.next();
-            if(resp=="si")
+            System.out.println("Imagen con parrafos de libro");
+            String resp=entrada.next();
+            if(resp.equals("si"))
             {
                 listaActividades.add("Leer parrafos de libros.");
             }
-            System.out.println("Imagen que represente musica relajante")
+            System.out.println("Imagen que represente musica relajante");
             resp=entrada.next();
-            if(resp=="si")
+            if(resp.equals("si"))
             {
                 listaActividades.add("Reproducir musica relajante, acompañada de imagenes");
                 
             }
             System.out.println("Imagen representativa de bebida caliente y cigarro");
             //EL CIGARRO ES DAÑINO PARA LA SALUD; PERO RELAJA AL USUARIO EN SI SI ESTE FUMA 
-            if(resp=="si")
+            if(resp.equals("si"))
             {
                 listaActividades.add("Un momento de descanso, con un cigarro y un cafe, alejado del computador.");
             }
             System.out.println("Imagen representativa de caminata.");
-            if(resp=="si")
+            if(resp.equals("si"))
             {
                 listaActividades.add("Una caminata por su sector");
             }
-            System.out.println("Imagen que represente el tocar musica")
-            if(resp=="si")
+            System.out.println("Imagen que represente el tocar musica");
+            if(resp.equals("si"))
             {
-                listaActividades.add("Interpretar canciones para distraerse.")
+                listaActividades.add("Interpretar canciones para distraerse.");
             }
             
             //aquí hay que colocar imagenes en una ventana, creo que queda mejor así que ir preguntando uno por uno y que el usuario
             //decida si o no. Supuestamente lo veremos mas adelante, asi que se puede hacer
-            
-            
             
             //La imagen podria guardar datos en el arraylist que instancie arriba, para que sea mas facil obtener los topes luego(usando el arraylist.size y todo eso)
         }
@@ -240,11 +236,12 @@ static boolean cierreTope=false;
             }//validar el ingreso
             return tiempo_usu;
         }
+
         private static double tiempo_descanso(double topeTrabajo)
         {
-            if (topeTrabajo<2.5) {
-                
-            System.out.println("Ahora elija el tiempo de descanso luego de su actividad de trabajo");  
+            if (topeTrabajo<2.5) 
+            {
+                System.out.println("Ahora elija el tiempo de descanso luego de su actividad de trabajo");  
                 System.out.println("Puede variar entre 10 y 30 minutos.");
                 int aux =45676875;
                 while(validarInt(aux,10,30))
@@ -252,7 +249,7 @@ static boolean cierreTope=false;
                     aux =entrada.nextInt();
                 }
                 return aux;
-            }
+                }
             else
             {
                 return 30;
@@ -283,6 +280,7 @@ static boolean cierreTope=false;
             double pDecimal=total_act-(int)total_act;
             setTopeTotal((int)total_act*60+(int)(pDecimal*60));
         }
+
         private static boolean validarDouble(double numero,double topeBajo, double topeAlto)
         {
             if(numero<topeBajo&&numero>topeAlto)
@@ -294,9 +292,6 @@ static boolean cierreTope=false;
             {
                 return false;
             } 
-                
-            
-            
         }
         
         private static boolean validarInt(int numero,int topeBajo,int topeAlto)
@@ -318,10 +313,14 @@ static boolean cierreTope=false;
         {
               Pattern pat = Pattern.compile(buscar);
               Matcher mat = pat.matcher(cadena);
-              if (mat.matches()) {
+              if (mat.matches()) 
+                {
                      return true;
-               } else {
+                } 
+                else 
+                {
                    return false;
-               }
+                }
         }
-    }
+}
+
