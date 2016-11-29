dcdff11c5eff6778e136c68a922b6d1ba1be1c51
felipevazo <f.vasquez07@ufromail.cl>
2016-11-28 23:19:30 -0300

    Update LeeFichero.java

--- LeeFichero.java
+++ LeeFichero.java
@@ -20,6 +20,7 @@ class LeeFichero {
     
    public boolean lee()
    {
+          
 
       try {
          // Apertura del fichero y creacion de BufferedReader para poder
@@ -56,6 +57,7 @@ class LeeFichero {
    }
    public void pasarArray()
    {
+          //Traspasa la informacion del TXT a un ArrayList llamado aux
         try {
          // Apertura del fichero y creacion de BufferedReader para poder
          // hacer una lectura comoda (disponer del metodo readLine()).
@@ -86,24 +88,25 @@ class LeeFichero {
 }
    public String devolverString(int pos)
    {
+          //Variable ingreso: posicion del array aux
+          //Retorna: el String que corresponda a esa posición
        return aux.get(pos);
    }
    public int devolverTamaño()
    {
+          //Devuelve Tamaño del array aux
        return aux.size();
    }
    public void ArrayNumeros()
    {
+          //Traspasa solo los numeros del txt a un array aparte llamado numeros
        
    
         try {
-         // Apertura del fichero y creacion de BufferedReader para poder
-         // hacer una lectura comoda (disponer del metodo readLine()).
     
          fr = new FileReader (archivo);
          br = new BufferedReader(fr);
 
-         // Lectura del fichero
          String linea;
          while((linea=br.readLine())!=null)
             numeros.add(linea);
@@ -111,9 +114,6 @@ class LeeFichero {
       catch(Exception e){
          e.printStackTrace();
       }finally{
-         // En el finally cerramos el fichero, para asegurarnos
-         // que se cierra tanto si todo va bien como si salta 
-         // una excepcion.
          try{                    
             if( null != fr ){   
                fr.close();     
@@ -127,41 +127,46 @@ class LeeFichero {
 
     public ArrayList<String> getAux() {
         return aux;
+           //retorna ArrayList aux
     }
 
     public void setAux(ArrayList<String> aux) {
         this.aux = aux;
+           //setArrayList(aux)
     }
 
     public ArrayList<String> getNumeros() {
         return numeros;
+           //Retorna ArrayList numeros
     }
 
     public void setNumeros(ArrayList<String> numeros) {
         this.numeros = numeros;
+           //SetArrayList(aux)
     }
     public String devolverNumero(int pos)
-    {
+    {// Variable entrada: posicion en ArrayList numeros
         ArrayNumeros();
         return this.numeros.get(pos);
+           //retorna: numero en esa posicion.
+           
     }
     public String devolverLinea(int pos)
     {
+           //Devuelve lineas del ArrayList Libro
         return Libro.get(pos);
     }
     public void devolverLibro()
     {
-        //int i=(int) (Math.random() * 14 + 1);
+           //Toma un Libro al azar de la carpeta Libros y lo pasa al ArrayList Libro
+        int i=(int) (Math.random() * 14 + 1);
        
-        File arch=new File("text"+0+".txt");
+        File arch=new File("text"+i+".txt");
           try {
-         // Apertura del fichero y creacion de BufferedReader para poder
-         // hacer una lectura comoda (disponer del metodo readLine()).
     
          fr = new FileReader (arch);
          br = new BufferedReader(fr);
 
-         // Lectura del fichero
          String linea;
          while((linea=br.readLine())!=null)
             Libro.add(linea);
@@ -169,9 +174,6 @@ class LeeFichero {
       catch(Exception e){
          e.printStackTrace();
       }finally{
-         // En el finally cerramos el fichero, para asegurarnos
-         // que se cierra tanto si todo va bien como si salta 
-         // una excepcion.
          try{                    
             if( null != fr ){   
                fr.close();     
