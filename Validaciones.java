1cdf77211f11e1b4f953d137f0b6ad13c2d1ac82
felipevazo <f.vasquez07@ufromail.cl>
2016-11-29 22:04:05 -0300

    Update Validaciones.java

--- Validaciones.java
+++ Validaciones.java
@@ -17,6 +17,11 @@ public class Validaciones {
     LeeFichero lee=new LeeFichero();
     
      public boolean confirmarDoubles(double topeTrabajo,int topeTotal, double topeDescanso) {
+         //Ingresan double,int,double
+         //Retorna boolean si se cumple:
+         //primer doble entre 1 y 4
+         //int entre 4 y 10
+         //segundo double entre 10 y 30
         boolean a=(topeTrabajo<=4&&topeTrabajo>=1);
         
         boolean b=(topeTotal<=10&&topeTotal>=4);
@@ -32,6 +37,9 @@ public class Validaciones {
     }
       public  boolean buscaLinea(String cadena,String buscar)
         {
+          //Variable ingreso: String,String
+          //Revisa si el segundo String esta dentro del primero
+          //Retorna boolean
               Pattern pat = Pattern.compile(buscar);
               Matcher mat = pat.matcher(cadena);
               if (mat.matches()) 
@@ -58,6 +66,9 @@ public class Validaciones {
         }
      public double stringDouble(String str)
         {
+         //Recibe String 
+         //Si string es numero double, lo retorna como double
+         //Si no, retorna 0.0
             boolean a=true;
            
   
@@ -83,6 +94,9 @@ public class Validaciones {
         }
      public boolean esNumero(String str)
      {
+         //Recibe str
+         //Si str es numero retorna true
+         //Si no, false
          try  
         {  
           double d = Double.parseDouble(str);  
